b448a313012512f89c371183f0430304725bc001
YourGitHubUsername <youremail@example.com>
2025-09-20 23:35:51 +0100

    your message

--- alx-project-0x00/interfaces/index.ts
+++ alx-project-0x00/interfaces/index.ts
@@ -1,3 +1,13 @@
 export interface PillProps {
     title: string
   }
+
+
+
+
+interface ButtonProps {
+    title: string;
+    styles?: string; // optional, for custom Tailwind classes
+    sizes: string; // e.g., 'small', 'medium', 'large'
+    shapes: string
+  }
