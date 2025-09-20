e0db2032885d659a567cf3604eaaf4e09326159b
YourGitHubUsername <youremail@example.com>
2025-09-20 23:29:35 +0100

    Added new files

--- alx-project-0x00/pages/index.tsx
+++ alx-project-0x00/pages/index.tsx
@@ -1,35 +1,16 @@
-// import Button from "@/components/Button";
-// const Home: React.FC = () => {
-//   return (
-//     <main className="flex flex-col h-screen justify-center items-center text-4xl font-semibold">
-//       <h1>Airbnb Application Clone system</h1>
-//       {/* <button className=" border px-3 py-1 text-lg mt-3  bg-blue-500 text-white rounded-full">Get Started</button> */}
-//       <Button sizes="small" shapes="rounded-full" title="Get Started"/>
-//     </main>
-//   )
-// }
-
-// export default Home;
-
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-import React from "react";
 import Button from "@/components/Button";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+export interface ButtonProps {
+  title: string;
+  styles?: string; // optional, for custom Tailwind classes
+  sizes: string; // e.g., 'small', 'medium', 'large'
+  shapes: string
+}
 
 const Home: React.FC = () => {
   return (
-    <main className="flex flex-col h-screen items-center text-4xl font-semibold">
+    <main className="flex flex-col h-screen justify-center items-center text-4xl font-semibold">
       <h1>Airbnb Application Clone system</h1>
+      {/* <button className=" border px-3 py-1 text-lg mt-3  bg-blue-500 text-white rounded-full">Get Started</button> */}
       <Button sizes="small" shapes="rounded-full" title="Get Started"/>
     </main>
   )
