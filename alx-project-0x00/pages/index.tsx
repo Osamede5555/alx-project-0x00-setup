8c8926a17627c3c647592b6c79139675bf01eb8a
YourGitHubUsername <youremail@example.com>
2025-09-20 23:32:10 +0100

    Updated index.tsx

--- alx-project-0x00/pages/index.tsx
+++ alx-project-0x00/pages/index.tsx
@@ -1,17 +1,9 @@
-import Button from "@/components/Button";
-export interface ButtonProps {
-  title: string;
-  styles?: string; // optional, for custom Tailwind classes
-  sizes: string; // e.g., 'small', 'medium', 'large'
-  shapes: string
-}
 
 const Home: React.FC = () => {
   return (
     <main className="flex flex-col h-screen justify-center items-center text-4xl font-semibold">
       <h1>Airbnb Application Clone system</h1>
-      {/* <button className=" border px-3 py-1 text-lg mt-3  bg-blue-500 text-white rounded-full">Get Started</button> */}
-      <Button sizes="small" shapes="rounded-full" title="Get Started"/>
+      <button className=" border px-3 py-1 text-lg mt-3  bg-blue-500 text-white rounded-full">Get Started</button>
     </main>
   )
 }
