94b67006883e16261a6698fc602f778b424a13c5
YourGitHubUsername <youremail@example.com>
2025-09-20 23:14:46 +0100

    your message

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -7,8 +7,15 @@ const Landing: React.FC =  () => {
         <Card />
         <div className="flex gap-4">
         <Button title="Rounded-sm" styles="px-4 py-2 rounded-sm" />
-        <Button title="Rounded-md" styles="px-4 py-2 rounded-md" />
-        <Button title="Rounded-full" styles="px-4 py-2 rounded-full" />
+<Button title="Rounded-md" styles="px-4 py-2 rounded-md" />
+<Button title="Rounded-full" styles="px-4 py-2 rounded-full" />
+
+      </div>
+
+      <div className="flex gap-4">
+        <Button title="Small" styles="text-sm px-3 py-1 rounded-md" />
+        <Button title="Medium" styles="text-base px-4 py-2 rounded-md" />
+        <Button title="Large" styles="text-lg px-6 py-3 rounded-md" />
       </div>
 
       </div>
