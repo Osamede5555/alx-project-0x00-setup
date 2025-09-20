2a7f4cb6317f8c14f3f605bbfc1338a8c3bef49a
YourGitHubUsername <youremail@example.com>
2025-09-20 23:11:46 +0100

    your message

---
+++ alx-project-0x00/components/Button.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+interface ButtonProps {
+  title: string;
+  styles?: string; // optional, for custom Tailwind classes
+}
+
+const Button: React.FC<ButtonProps> = ({ title, styles }) => {
+  return (
+    <button
+      className={`px-4 py-2 font-semibold text-white bg-blue-600 hover:bg-blue-700 transition ${styles}`}
+    >
+      {title}
+    </button>
+  );
+};
+
+export default Button;
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,9 +1,16 @@
 import Card from "@/components/Card"
+import Button from "../components/Button";
 const Landing: React.FC =  () => {
     return (
       <div>
         <h1 className=" text-xl font-extralight">Landing Page</h1>
         <Card />
+        <div className="flex gap-4">
+        <Button title="Rounded-sm" styles="px-4 py-2 rounded-sm" />
+        <Button title="Rounded-md" styles="px-4 py-2 rounded-md" />
+        <Button title="Rounded-full" styles="px-4 py-2 rounded-full" />
+      </div>
+
       </div>
     )
   }
